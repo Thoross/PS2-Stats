f7a60aa2d0c80cceaea8b8b78e543d1f3225b0de
Thoross <brendan.betts@live.com>
2013-06-07 09:45:34 -0300

    Updated license information on api.py and test_get_character_id_by_name.py

--- src/api.py
+++ src/api.py
@@ -1,7 +1,16 @@
 '''
-Created on Jun 6, 2013
+PS2-Stats : Python module for PlanetSide 2 Stat tracking.
 
-@author: Brendan Betts
+Copyright (C) 2013 Brendan Betts (brendan.betts@live.com)
+
+License: GNU LGPL
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+Created on Jun 6, 2013
 '''
 
 import requests
--- tests/test_get_character_id_by_name.py
+++ tests/test_get_character_id_by_name.py
@@ -1,8 +1,18 @@
 '''
-Created on Jun 6, 2013
+PS2-Stats : Python module for PlanetSide 2 Stat tracking.
 
-@author: Acedia
+Copyright (C) 2013 Brendan Betts (brendan.betts@live.com)
+
+License: GNU LGPL
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+Created on Jun 6, 2013
 '''
+
 import unittest
 import api
 

4f93869563604f45dc4b30eb18006ba95ca4811f
bbetts <bbetts@salesforce.com>
2013-06-25 16:23:07 -0300

    Stripped api of code that will be replaced. Going to do a rewrite of it eventually.

--- src/api.py
+++ src/api.py
@@ -16,57 +16,8 @@ Created on Jun 6, 2013
 import requests
 from src.url import url
 
-URL_ROOT = "http://census.soe.com/get/ps2-beta/"
 
-
-
-def do_request(request_url):
-    r = requests.get(request_url)
-    return r.json()
-
-def get_character_id_by_name(name):
-    parameters = "character/?name.first_lower=%s&c:show=id" %(name)
-    request_url = url(parameters)
-    obj = do_request(request_url.full)
-    try:
-        return obj["character_list"][0]["id"]
-    except IndexError, e:
-        return "None" 
-    
-def get_certs_by_id(charcter_id):
-    parameters = "character/%s?c:show=certs" %(character_id)
-    request_url = url(parameters)
-    obj = do_request(request_url.full)
-    try:
-        certs = {"certs_earned": obj["character_list"][0]["certs"]["currentpoints"],"percentage_to_next": obj["character_list"][0]["certs"]["percentagetonext"]}
-        return certs
-    except:
-        return
-
-def get_resources(character_id):
-    request_url = url("character/%s?c:show=currency" %(character_id))
-    obj = do_request(request_url.full)
-    try:
-        resources = {"aerospace":obj["character_list"][0]["currency"]["aerospace"], "infantry" :obj["character_list"][0]["currency"]["infantry"], "mechanized":obj["character_list"][0]["currency"]["mechanized"] }
-        return resources
-    except:
-        return    
-def get_experience(character_id):
-    request_url = url("character/%s?c:show=experience" %(character_id))
-    obj = do_request(request_url.full)
-    try:
-        experience = {"level" : obj["character_list"][0]["experience"][0]["rank"], "score" : obj["character_list"][0]["experience"][0]["score"]}
-        return experience
-    except:
-        return
-        
-if __name__ == "__main__":
-    character_id = get_character_id_by_name("sorter")
-    print "Character ID: %s" %(character_id)
-    certs = get_certs_by_id(character_id)
-    print "Certs Earned: %s \nPercentage to Next: %s" %(certs['certs_earned'],certs['percentage_to_next'])
-    resources = get_resources(character_id)
-    print "Resources:\n  Aerospace: %s \n  Infantry: %s \n  Mechanized: %s" %(resources["aerospace"],resources["infantry"],resources["mechanized"])
-    experience = get_experience(character_id)
-    print "Experience: \n  Level: %s\n  Score: %s" %(experience["level"],experience["score"])
-    
+def do_request(request_parameters):
+    request_url = url(request_parameters)
+    r = requests.get(request_url.full)
+    return r.json()   

98386fafc2d1907eb103a9267ab8224da3af0f8d
Thoross <brendan.betts@live.com>
2013-06-24 22:06:54 -0300

    Created the Character, Vehicle and Weapon classes.

---
+++ src/Character.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 24, 2013
+
+@author: Acedia
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+    
+
+    def __init__(self,id):
+        '''
+        Constructor
+        '''
+        self.id = id
+        
---
+++ src/Weapon.py
@@ -0,0 +1,17 @@
+'''
+Created on Jun 24, 2013
+
+@author: Acedia
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,id):
+        '''
+        Constructor
+        '''
+        self.id = id
